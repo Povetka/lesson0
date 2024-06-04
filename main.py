3648a6e98cc33205e8abef6844d68b36fa5d9106
Povetka <vorobyshek9222@gmail.com>
2024-06-05 00:10:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+a = ('qwerty')
+print(len(a))
+first = 7
+second = 4
+summa = first + second
+diff = first - second
+print(summa, diff)
+third = 9
+numbers = first, second, third
+mean = sum(numbers) // 3
+print(mean)
+first_string = ('Вторник')
+second_string = ('Понедельник')
+print(second_string,', ', first_string)
+a1 = 5
+b = 8
+c = 2
+f = (((a1*b)+(a1*c))**3)/2
+print(f)
