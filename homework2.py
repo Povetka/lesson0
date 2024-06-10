e3cbdecb72851572ab3dba3367fac6a06e7c385a
Povetka <vorobyshek9222@gmail.com>
2024-06-10 18:17:51 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+time_spent = 1.5
+course_name = 'Python'
+time_for_one_task = time_spent / completed_homework
+print('Курс: ', course_name, ', ', 'всего задач: ', completed_homework, ', ', 'затрачено часов: ', time_spent, ',', 'среднее время выполнения: ', time_for_one_task, 'часа.')
