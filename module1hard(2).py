b2baa20c754ac8717a3c285c28c7e5030cc9bcff
Povetka <vorobyshek9222@gmail.com>
2024-06-12 22:11:56 +0300

    Update module1hard(2).py

--- module1hard(2).py
+++ module1hard(2).py
@@ -3,5 +3,5 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 sort_students = sorted(students)
 grades_new = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
               sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
-res = dict(zip(sort_students, grades_new))
-print(res)
+grade_book = dict(zip(sort_students, grades_new))
+print(grade_book)
