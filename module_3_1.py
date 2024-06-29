30349e0095a3cbd7e003cbc86a3959adcfcf2491
Povetka <vorobyshek9222@gmail.com>
2024-06-29 15:40:01 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    a = len(string)
+    b = string.upper()
+    c = string.lower()
+    return a, b, c
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if string.lower() in i.lower() or i.lower() in string.lower():
+            # уточнить, почему в строке выше ищем в i в нижнем регистре, а не в list_
+            return True
+        else:
+            return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
