1c26b867cbad2fdf634941d27f7b10197ef6f40a
Povetka <vorobyshek9222@gmail.com>
2024-07-08 01:19:38 +0300

    Add files via upload

---
+++ directory_module_4_1/fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    if second != 0:
+        result_div = first / second
+        return result_div
---
+++ directory_module_4_1/true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    if second != 0:
+        result_div = first / second
+        return result_div
