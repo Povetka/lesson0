111092eedabdaeef9cb6d2173f8d01a661a27bb1
Povetka <vorobyshek9222@gmail.com>
2024-07-20 03:18:54 +0300

    Update module_5_4.py

--- module_5__1_and_2_and_3_and_4/module_5_4.py
+++ module_5__1_and_2_and_3_and_4/module_5_4.py
@@ -1,4 +1,64 @@
-from module_5_1 import House
+class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('"Такого этажа не существует"')
+        else:
+            elevator_path = range(1, new_floor + 1)
+            return print(*elevator_path, sep='\n')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors == other.number_of_floors)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors < other.number_of_floors)
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors <= other.number_of_floors)
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors > other.number_of_floors)
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors >= other.number_of_floors)
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return bool(self.number_of_floors != other.number_of_floors)
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self) :
+        print(f'"{self.name} снесён, но он останется в истории"')
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -14,8 +74,6 @@ del h3
 
 print(House.houses_history)
 
-# Тоже, чтобы не копировать код класса дополнила класс методами нужными для решения задания
-# и импортировала его из модуля где он прописан.
 
 # Текст дз
 #
