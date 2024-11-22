dc152af5ba4183e42c11fe2dc9741888edbafe6a
Povetka <vorobyshek9222@gmail.com>
2024-11-22 18:35:34 +0300

    Add files via upload

---
+++ модуль_16_Библиотеки_для_работы_с_ресурсами_1_1/module_16_4.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, status, Body, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+@app.get("/users")
+def get_all_users() -> List[User]:
+    return users
+
+
+@app.post("/user/{username}/{age}")
+def create_user(username: str, age: int) -> User:
+    user = User(id=len(users) + 1, username=username, age=age)
+    users.append(user)
+    return user
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+def update_user(user_id: int, username: str, age: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+# Домашнее задание по теме "Модели данных Pydantic"
+#
+# Цель: научиться описывать и использовать Pydantic модель.
+#
+# Задача "Модель пользователя":
+# Подготовка:
+# Используйте CRUD запросы из предыдущей задачи.
+# Создайте пустой список users = []
+# Создайте класс(модель) User, наследованный от BaseModel, который будет содержать следующие поля:
+# id - номер пользователя (int)
+# username - имя пользователя (str)
+# age - возраст пользователя (int)
+#
+# Измените и дополните ранее описанные 4 CRUD запроса:
+# get запрос по маршруту '/users' теперь возвращает список users.
+# post запрос по маршруту '/user/{username}/{age}', теперь:
+# Добавляет в список users объект User.
+# id этого объекта будет на 1 больше, чем у последнего в списке users. Если список users пустой, то 1.
+# Все остальные параметры объекта User - переданные в функцию username и age соответственно.
+# В конце возвращает созданного пользователя.
+# put запрос по маршруту '/user/{user_id}/{username}/{age}' теперь:
+# Обновляет username и age пользователя, если пользователь с таким user_id есть в списке users и возвращает его.
+# В случае отсутствия пользователя выбрасывается исключение HTTPException с описанием "User was not found" и кодом 404.
+# delete запрос по маршруту '/user/{user_id}', теперь:
+# Удаляет пользователя, если пользователь с таким user_id есть в списке users и возвращает его.
+# В случае отсутствия пользователя выбрасывается исключение HTTPException с описанием "User was not found" и кодом 404.
